5ef998a8a354ec4c8ff35f5149ece15cbcaaa8a3
Fakhri-P127 <fakhrita@code.edu.az>
2022-02-25 21:32:02 +0400

    Array-Sort/

--- Array-Sort/Array-Sort/Program.cs
+++ Array-Sort/Array-Sort/Program.cs
@@ -7,19 +7,19 @@ namespace Array_Sort
         static void Main(string[] args)
         {
             int[] numbers = { 15, 124, 12, -214, 99, 0, 121, -3133, 131, 341, 4241, -212 };
-
+          
             for (int m = 0; m < numbers.Length; m++)
-            {                        
-                for (int i = 1; i < numbers.Length; i++)
+            {
+                for (int i = 0; i < numbers.Length-1; i++)
                 {
-                    if (numbers[i - 1] > numbers[i])
-                    {                        
-                        int copyOfFirst = numbers[i];
-                        numbers[i] = numbers[i-1];
-                        numbers[i-1] = copyOfFirst;
+                    if (numbers[i] > numbers[i+1])
+                    {
+                        int cloneOfFirst = numbers[i];
+                        numbers[i] = numbers[i+1];
+                        numbers[i+1] = cloneOfFirst;
                     }
                 }
-            }
+            }           
 
             foreach (int number in numbers)
             {
